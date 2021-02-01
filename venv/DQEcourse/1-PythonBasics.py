6e78310894730aee25bcc19a7eea0bf281460875
Oleg_Krylov <krylov_o@mail.ru>
2021-02-01 08:40:56 +0300

    Comments added

--- venv/DQEcourse/1-PythonBasics.py
+++ venv/DQEcourse/1-PythonBasics.py
@@ -2,37 +2,38 @@
 import random
 
 # define length of the list
-dieLaenge = 1000
-# create list with 1, 2, ..., length in it
+dieLaenge = 100
+# define the Depth of probability
+WahrscheinlichkeitTiefe = 1000
+# create list with sequence 1, 2, ..., length in it
 dieListe = list(range(0, dieLaenge))
-# create auxiliary list for cycle
+# create auxiliary list for ordering in cycle
 dieReihenfolge = list(range(0, dieLaenge))
-# create/define dummy for sorted list
+# create/define dummy list for sorted list
 sortierteListe = list(range(0, dieLaenge))
 # form list with random number
 for i in dieReihenfolge:
-    dieListe[i] = random.randrange(0, 1000)
+    dieListe[i] = random.randrange(0, WahrscheinlichkeitTiefe)
 # an output for random list
 print("------->: unsorted random List:")
 print(dieListe)
 print("------->sorted random List:")
+#sorting the List
 Tiefstwert = 0
-Maximalwert = 1000
+Maximalwert = WahrscheinlichkeitTiefe
 j = 0
 while j < dieLaenge:
     Tiefstwert = min(dieListe)
-    # Maximalwert = max(dieListe)
     sortierteListe[j] = Tiefstwert
     dieListe.remove(Tiefstwert)
     j = j + 1
 
-print(sortierteListe)
-
-# even
+# sum of even
 geradzahlige = 0
-# odd
+# sum of odd
 ungerade = 0
 
+# calculation of sums for even and odd numbers
 k = 0
 while k < dieLaenge:
     if sortierteListe[k] % 2 == 0:
@@ -41,5 +42,9 @@ while k < dieLaenge:
         ungerade = ungerade + sortierteListe[k]
     k = k + 1
 
+print(sortierteListe)
+
+print("The sum of even numbers:")
 print(geradzahlige)
+print("The sum of odd numbers:")
 print(ungerade)
